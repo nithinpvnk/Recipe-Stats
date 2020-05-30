0aa5fdae9c266df4681ab79391e80a8d573a9e44
Nithin Kumar <nithin.pvnk@gmail.com>
2020-05-30 19:39:29 -0400

    Layouts addition
    Added a couple of layouts

--- app/src/main/java/com/techdining/www/recipestats/viewModel/RecipeHomeViewModel.kt
+++ app/src/main/java/com/techdining/www/recipestats/viewModel/RecipeHomeViewModel.kt
@@ -1,7 +1,15 @@
 package com.techdining.www.recipestats.viewModel
 
 import androidx.lifecycle.ViewModel
+import timber.log.Timber
 
 class RecipeHomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    init {
+        Timber.i("Recipe Homepage ViewModel is created")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Timber.i("Recipe Homepage ViewModel is Destroyed")
+    }
 }
